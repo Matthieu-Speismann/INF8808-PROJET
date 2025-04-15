5d0cf9c3ed7811b76fcf79cc4ff6fd451b867a53
Matthieu Speismann <matt.speismann@gmail.com>
2025-04-15 12:19:05 -0400

    Last Dance

--- project/app.py
+++ project/app.py
@@ -8,6 +8,7 @@ from visualisation_4.app import get_viz_4_html
 from visualisation_5.app import get_viz_5_html
 
 app = dash.Dash(__name__)
+server = app.server 
 app.title = "Projet INF8808"
 
 app.layout = html.Div([
