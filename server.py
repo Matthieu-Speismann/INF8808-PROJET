7fe4ef05c0d583b429fa350d3474afcb4310c69b
Matthieu Speismann <matt.speismann@gmail.com>
2025-04-15 12:30:04 -0400

    Sortie pour site en local

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask_failsafe import failsafe
+
+@failsafe
+def create_app():
+    from app import app
+    return app.server
+
+if __name__ == "__main__":
+    create_app().run(host="0.0.0.0", port=8050, debug=False)
